915fcce8c1270bc14cb3c8238a9456ce0a953130
Héctor Sansano Miralles <sansanomiralles@gmail.com>
2017-03-08 19:05:14 +0100

    Almacen

---
+++ Almacen/src/org/example/www/servicioalmacen/ServicioAlmacenSkeleton.java
@@ -0,0 +1,49 @@
+
+/**
+ * ServicioAlmacenSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
+ */
+    package org.example.www.servicioalmacen;
+    /**
+     *  ServicioAlmacenSkeleton java skeleton for the axisService
+     */
+    public class ServicioAlmacenSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param comprobarStock 
+             * @return comprobarStockResponse 
+         */
+        
+                 public org.example.www.servicioalmacen.ComprobarStockResponse comprobarStock
+                  (
+                  org.example.www.servicioalmacen.ComprobarStock comprobarStock
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#comprobarStock");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param actualizarStock 
+             * @return actualizarStockResponse 
+         */
+        
+                 public org.example.www.servicioalmacen.ActualizarStockResponse actualizarStock
+                  (
+                  org.example.www.servicioalmacen.ActualizarStock actualizarStock
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#actualizarStock");
+        }
+     
+    }
+    
