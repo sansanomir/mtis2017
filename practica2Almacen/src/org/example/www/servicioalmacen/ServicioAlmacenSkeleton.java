b0bc6312052ee65f0d6ed9ba17b85261a754b618
Héctor Sansano Miralles <sansanomiralles@gmail.com>
2017-03-08 19:49:14 +0100

    Práctica 2 comienzo

---
+++ practica2Almacen/src/org/example/www/servicioalmacen/ServicioAlmacenSkeleton.java
@@ -0,0 +1,32 @@
+
+/**
+ * ServicioAlmacenSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
+ */
+    package org.example.www.servicioalmacen;
+    /**
+     *  ServicioAlmacenSkeleton java skeleton for the axisService
+     */
+    public class ServicioAlmacenSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param comprobarStock 
+             * @return comprobarStockResponse 
+         */
+        
+                 public org.example.www.servicioalmacen.ComprobarStockResponse comprobarStock
+                  (
+                  org.example.www.servicioalmacen.ComprobarStock comprobarStock
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#comprobarStock");
+        }
+     
+    }
+    
---
+++ practica2Proveedor1/src/org/example/www/servicioproveedor/ServicioProveedorSkeleton.java
@@ -0,0 +1,32 @@
+
+/**
+ * ServicioProveedorSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
+ */
+    package org.example.www.servicioproveedor;
+    /**
+     *  ServicioProveedorSkeleton java skeleton for the axisService
+     */
+    public class ServicioProveedorSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param solicitarPresupuesto 
+             * @return solicitarPresupuestoResponse 
+         */
+        
+                 public org.example.www.servicioproveedor.SolicitarPresupuestoResponse solicitarPresupuesto
+                  (
+                  org.example.www.servicioproveedor.SolicitarPresupuesto solicitarPresupuesto
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#solicitarPresupuesto");
+        }
+     
+    }
+    
