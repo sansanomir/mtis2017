38cf1643aee5be7a42b8ee417b796746ccca669f
Héctor Sansano Miralles <sansanomiralles@gmail.com>
2017-03-18 19:01:28 +0100

    Práctica 2 Servicio Proveedor

--- p21/src/org/example/www/p1/P1Skeleton.java
+++ p21/src/org/example/www/p1/P1Skeleton.java
@@ -20,13 +20,31 @@ import com.mysql.jdbc.PreparedStatement;
     public class P1Skeleton{
     	private String url= null;
     	private Connection con = null;
+    	private Connection conA = null;
     	
     	public P1Skeleton() throws ClassNotFoundException, SQLException{
     		url = "jdbc:mysql://localhost:3311/proveedor1";
        	 	Class.forName("com.mysql.jdbc.Driver");
        	 	con = (Connection)DriverManager.getConnection(url,"root","root");
+       	 	conA = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3311/almacen","root","root");
     	}
         
+    	private int cantidadProductoAlmacen(String referencia){
+    		int cantidad=-1;
+    		String sql = "select * from producto where referenciaProducto = '"
+               	 + referencia+"'";
+        	 try (PreparedStatement stmt = (PreparedStatement) conA.prepareStatement(sql)) {
+        		  ResultSet rs = stmt.executeQuery();  
+        		  while (rs.next()){
+        			  cantidad = rs.getInt("producto.stock");
+        		  }           		                    		 
+        		} catch (SQLException sqle) { 
+        		  System.out.println("Error en la ejecución:" 
+        		    + sqle.getErrorCode() + " " + sqle.getMessage());    
+        		}
+    		return cantidad;
+    	}
+    	
     	private float getPrecioProducto(String referencia){
         	float precio = -1;
         	String sqlc = "select * from producto where referenciaProducto = '"
@@ -34,7 +52,7 @@ import com.mysql.jdbc.PreparedStatement;
 	       	 try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sqlc)){
 	        		  ResultSet rs = stmt.executeQuery();           		 
 	        		  while (rs.next()){
-	        			 precio= rs.getInt("precio");
+	        			 precio= rs.getFloat("precio");
 	        			 return precio;
 	        		  }           		                    		 
 	        		} catch (SQLException sqle) { 
@@ -106,7 +124,7 @@ import com.mysql.jdbc.PreparedStatement;
                   )
             {
                 	 SolicitarPresupuestoResponse resp = new SolicitarPresupuestoResponse();
-                	 resp.setOut(-1);
+                	 resp.setOut(9999999);
                 	 String sql = "select * from producto where referenciaProducto = '"
                 	 + solicitarPresupuesto.getReferenciaProducto()+"'";
                  	 try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)) {
@@ -139,28 +157,30 @@ import com.mysql.jdbc.PreparedStatement;
                   )
             {
                 	 OrdenarCompraResponse resp = new OrdenarCompraResponse();
-                	 resp.setOut(false);
-                	 
-                	 int cantidad=-1;
-                	 String sqlc = "select * from producto where referenciaProducto = '"
-                        	 + ordenarCompra.getReferenciaProducto()+"'";
-                	 try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sqlc)){
-                 		  ResultSet rs = stmt.executeQuery();           		 
-                 		  while (rs.next()){
-                 			 cantidad= rs.getInt("stock");
-                 		  }           		                    		 
-                 		} catch (SQLException sqle) { 
-                 		  System.out.println("Error en la ejecución:" 
-                 		    + sqle.getErrorCode() + " " + sqle.getMessage());    
-                 		}
-                	 if(cantidad == -1){
+                	 resp.setOut(false);               	 
+                	 int cantAlmacen = cantidadProductoAlmacen(ordenarCompra.getReferenciaProducto());
+                	 int cantidad=-1;               	 
+                	 if(cantAlmacen != 0){
+                		 String sqlc = "INSERT INTO producto"+
+                	 "(referenciaProducto,precio, stock) VALUES(?,?,?)";
+                		 try (PreparedStatement stmt = (PreparedStatement) conA.prepareStatement(sqlc)){
+                			 stmt.setString(1,ordenarCompra.getReferenciaProducto());
+                			 stmt.setFloat(2, getPrecioProducto(ordenarCompra.getReferenciaProducto()));
+                			 stmt.setInt(3,ordenarCompra.getNumeroUnidades());
+                			 stmt.executeUpdate();         		 
+                    		  resp.setOut(true);
+                    		  return resp;
+                    		} catch (SQLException sqle) { 
+                    		  System.out.println("Error en la ejecución:" 
+                    		    + sqle.getErrorCode() + " " + sqle.getMessage());    
+                    		}
                 		 return resp;
                 	 }
                 	 else{
                     	 String sql = "UPDATE producto SET stock = ? "
     			                  + " WHERE referenciaProducto = ?";               	 
-                     	 try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)) {
-                     		 stmt.setInt(1,cantidad-ordenarCompra.getNumeroUnidades());
+                     	 try (PreparedStatement stmt = (PreparedStatement) conA.prepareStatement(sql)) {
+                     		 stmt.setInt(1,cantAlmacen + ordenarCompra.getNumeroUnidades());
                      		 stmt.setString(2,ordenarCompra.getReferenciaProducto());
                      		 stmt.executeUpdate();
                      		 resp.setOut(true);
